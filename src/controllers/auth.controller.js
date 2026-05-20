50709061a546cff4fc757d931f0ebc78f7c68684
Miguel Angel <l22020805@veracruz.tecnm.mx>
2026-05-20 03:33:56 -0600

    cors

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -80,8 +80,8 @@ exports.login = async (req, res, next) => {
 
         const cookieOptions = {
             httpOnly: true,
-            secure: isProduction,
-            sameSite: isProduction ? 'None' : 'Lax',
+            secure: true,
+            sameSite: 'None',
             maxAge: 7 * 24 * 60 * 60 * 1000
         };
 
